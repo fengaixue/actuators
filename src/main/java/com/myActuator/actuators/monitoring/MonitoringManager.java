dfa10163ea766d69bf5b45c7a4ef9f653be3e1e0
fengaixue <fengaixue@users.noreply.github.com>
2020-09-18 13:52:45 +0800

    调整执行逻辑
    循环监测心跳由循环转为定时器

--- src/main/java/com/myActuator/actuators/monitoring/MonitoringManager.java
+++ src/main/java/com/myActuator/actuators/monitoring/MonitoringManager.java
@@ -2,6 +2,7 @@ package com.myActuator.actuators.monitoring;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,8 +12,8 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.net.ConnectException;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * <h1> 监听类 用户监控项目健康状态 </h1>
@@ -21,19 +22,48 @@ import java.util.Map;
  * @date 2020/09/16 16:46
  */
 @Component
+@Slf4j
 public class MonitoringManager implements CommandLineRunner {
 
     @Autowired
     private  RestTemplate  restTemplate;
+    /**
+     * bat 执行方法和路径
+     */
+    private  static final String CMDPATH = "cmd /c start D://mineJar/stop2.bat";
 
-    //private  static final String REMOTEHOST = "";
+    /**
+     * 需要监控的项目的地址
+     */
+    private  static final String REMOTHOST = "http://localhost:9099/actuator/health";
 
+    /**
+     * 主状态 KEY
+     */
+    private  static final String KEY_STATUS = "status";
 
+    /**
+     * 状态
+     */
+    private  static final String UP = "UP";
+
+    /**
+     * 初始化方法 设置循环周期、等待周期
+     */
+    public MonitoringManager(){
+
+    }
+
+    /**
+     * bat 执行方法 每次执行之后会休眠10分钟
+     * @param obj
+     * @throws Exception
+     */
     public void exeu(Object obj) throws  Exception{
-        String cmd = "cmd /c start D://mineJar" + "/stop2.bat";
-        Process ps = Runtime.getRuntime().exec(cmd);
+        log.info(" start time:{}",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        Process ps = Runtime.getRuntime().exec(CMDPATH);
         ps.waitFor();
-        System.out.println("执行了bat>> stop2");
+        log.info(" run restart bat :{}",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         //休息10分钟 用于启动项目时间
         Thread.sleep(1000 * 60 *  10);
     }
@@ -41,41 +71,63 @@ public class MonitoringManager implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Object obj=new Object();
-
         Thread t1=new Thread(new Runnable() {
             @Override
             public void run() {
-                //对象锁: notirty wait
-                // synchronized (obj) {
-                System.out.println("获取健康数据线程启动。。。。");
-                while (true){
-                    try {
-                        String jsonStr = restTemplate.getForObject("http://localhost:9099/actuator/health", String.class);
-                        System.out.println(jsonStr);
-                        JSONObject jsonObject = JSON.parseObject(jsonStr);
-                        String status = jsonObject.getString("status");
-                        if(!"UP".equals(status)){
-                            exeu(obj);
-                        }
-                    } catch (Exception e) {
-                        try{
-                            exeu(obj);
-                        }catch(Exception a){
-                            System.out.println(2);
-                            a.printStackTrace();
+                log.info("获取健康数据线程启动。。。。");
+                Calendar calendar = Calendar.getInstance();
+                Date firstTime = calendar.getTime();
+                //每10秒执行一次
+                long period = 1000 * 10 *  1;
+
+                Timer timer = new Timer();
+
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            String jsonStr = restTemplate.getForObject(REMOTHOST, String.class);
+                            log.info(jsonStr);
+                            JSONObject jsonObject = JSON.parseObject(jsonStr);
+                            String status = jsonObject.getString(KEY_STATUS);
+                            if(!UP.equals(status)){
+                                exeu(obj);
+                            }
+                        } catch (Exception e) {
+                            try{
+                                exeu(obj);
+                            }catch(Exception a){
+                                log.error("exeu run error >> {}",a.getMessage());
+                            }
                         }
                     }
-                    try{
-                        //每10秒执行一次
-                        Thread.sleep(1000 * 10 *  1);
-                    }catch(Exception e){
-                        System.out.println(1);
-                        e.printStackTrace();
-                    }
-                }
-                // }
+                }, firstTime, period);
             }
-        },"D1");
+        });
         t1.start();
     }
 }
+//多线程方式执行，对象锁，较为不妥，唤醒时间未按设定时间完成唤醒
+/* while (true){
+    try {
+        String jsonStr = restTemplate.getForObject(REMOTHOST, String.class);
+        //log.info(jsonStr);
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        String status = jsonObject.getString(KET_STATUS);
+        if(!UP.equals(status)){
+            exeu(obj);
+        }
+    } catch (Exception e) {
+        try{
+            exeu(obj);
+        }catch(Exception a){
+            log.error("exeu run error >> {}",a.getMessage());
+        }
+    }
+    try{
+        //每10秒执行一次
+        Thread.sleep(1000 * 10 *  1);
+    }catch(Exception e){
+        log.error("sleep run error >> {}",e.getMessage());
+    }
+}*/
