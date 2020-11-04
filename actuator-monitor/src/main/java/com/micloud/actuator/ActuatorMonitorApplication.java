60635c7f45f3e6d98fe0bc1e7ebfd60aa9085c2f
fengaixue <fengaixue@users.noreply.github.com>
2020-11-04 14:12:37 +0800

    项目架构调整

--- actuator-monitor/src/main/java/com/micloud/actuator/ActuatorMonitorApplication.java
+++ actuator-monitor/src/main/java/com/micloud/actuator/ActuatorMonitorApplication.java
@@ -5,14 +5,14 @@ import com.micloud.actuator.component.health.HealthEntity;
 import com.micloud.actuator.enums.MonitorEnums;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @SpringBootApplication
 @EnableScheduling
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ActuatorMonitorApplication {
 
 	public static void main(String[] args) {
--- actuator-zuul/src/main/java/com/micloud/actuator/MicloudZuulApplication.java
+++ actuator-zuul/src/main/java/com/micloud/actuator/MicloudZuulApplication.java
@@ -3,14 +3,16 @@ package com.micloud.actuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
 @EnableZuulProxy
-public class MicloudActuatorApplication {
+@EnableDiscoveryClient
+public class MicloudZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicloudActuatorApplication.class,args);
+        SpringApplication.run(MicloudZuulApplication.class,args);
     }
 
 }
